352d0bf3e6361a6523515846136ffec78baf0c5b
richter <richter@last.cl.uni-heidelberg.de>
2016-12-13 16:27:27 +0100

    testing feature extraction unigram

--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -17,8 +17,10 @@ class FeatureExtractor:
         # Extract lemmas
         lemmas = word_helper()
         pos_tags = ['NNP', 'NNS', 'DT', 'CD']
+        print("Defining lemma features")
         for lemma in lemmas:
             dict_features[lemma.lower().strip()] = 0
+        print("Creating POS-tag features")
         for pos in pos_tags:
             dict_features[pos] = 0
 
@@ -26,12 +28,15 @@ class FeatureExtractor:
         return dict_features
 
     def extract_baseline_features(self):
+        i = 0
         # Extracts: lemma, pos tags
 
         baseline_features = self.define_baseline_features()
         result = []
 
         for sample in self.ne_list:
+            i += 1
+            print("Processing sample no.: ", i)
             # List of lemmas in sample
             sample_lemmas = [lemma_list[0] for key, value in sample.items() for lemma_list in value if type(lemma_list) == list]
             # List of pos in sample
--- tests/test_feature_extractor.py
+++ tests/test_feature_extractor.py
@@ -13,7 +13,7 @@ from ml_ner.corpus.corpusreader import CorpusReader
 
 # /resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/train/data/english/annotations/nw/
 # Create an instance of the CorpusReader class
-cr = CorpusReader("/resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/train/data/english/annotations/nw/")
+cr = CorpusReader("/resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/development/data/english/annotations/nw/wsj")
 
 # Extract the NE, its POS tags and phrases
 ne = cr.extract_labeled_named_entities()

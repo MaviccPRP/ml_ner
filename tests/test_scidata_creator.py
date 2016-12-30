2d6e602edbc8f6bbf699a5c07ae5212d7ad830aa
mavicc <mavicc@web.de>
2016-12-30 18:48:34 +0100

    classification optimization

--- tests/test_scidata_creator.py
+++ tests/test_scidata_creator.py
@@ -8,13 +8,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
 import pprint
 from numpy import array
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 
 from ml_ner.feature_extraction.feature_extractor import FeatureExtractor
 from ml_ner.feature_extraction.arff_scikitdata_creator import ArffAndSciKitDataCreator
@@ -27,7 +30,7 @@ Extract features for the training set
 
 # /resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/train/data/english/annotations/nw/
 # Create an instance of the CorpusReader class
-cr = CorpusReader("/resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/train/data/english/annotations/nw/wsj")
+cr = CorpusReader("/resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/development/data/english/annotations/nw/wsj")
 
 # Extract the NE, its POS tags and phrases
 ne = cr.extract_labeled_named_entities()
@@ -66,8 +69,8 @@ data = ArffAndSciKitDataCreator(samples)
 
 X_test, y_test = array(data.createScikitData()[0]), array(data.createScikitData()[1])
 
-print("Classify one-vs-one", flush=True)
-
+print("Classifying", flush=True)
+'''
 for test_sample, entity, f_vector in zip(X_test, ne, samples):
     y_pred_l1_one = OneVsOneClassifier(LinearSVC(random_state=0, dual=False, penalty='l1')).fit(X_train, y_train).predict(test_sample.reshape(1, -1))
     for key, value in entity.items():
@@ -77,9 +80,48 @@ for test_sample, entity, f_vector in zip(X_test, ne, samples):
                 if not isinstance(value, str) and value > 0:
                     print(str(key) + " : " + str(value), flush=True)
             print("____________________________________________", flush=True)
+'''
 
-y_pred_l1_one = OneVsOneClassifier(LinearSVC(random_state=0, dual=False, penalty='l1')).fit(X_train, y_train).predict(X_test)
+tuned_parameters = [{'penalty': ['l1'], 'dual': [False], 'multi_class': ['ovr','crammer_singer'],
+                     'max_iter': [100,1000,10000], 'C': [1, 10, 100, 1000]},
+                    {'penalty': ['l2'], 'loss': ['hinge', 'squared_hinge'],'dual': [True], 'multi_class': ['ovr', 'crammer_singer'],
+                     'max_iter': [100, 1000, 10000], 'C': [1, 10, 100, 1000]}]
 
-print(accuracy_score(y_test, y_pred_l1_one))
-print(f1_score(y_test, y_pred_l1_one, average='weighted'))
-print(confusion_matrix(y_test, y_pred_l1_one, labels=['PERSON', 'GPE_NORP', 'ORG', 'DATE', 'PERCENT_CARDINAL_MONEY']))
+scores = ['accuracy', 'f1_weighted']
+
+labels=['PERSON', 'GPE_NORP', 'ORG', 'DATE', 'PERCENT_CARDINAL_MONEY']
+
+
+print("Classify LinearSVC")
+for score in scores:
+    clf = GridSearchCV(LinearSVC(), tuned_parameters, scoring='%s' % score)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_test, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_test, y_pred, labels=labels))
+    print()
+
+    '''
+    print("Solely LinearSVC")
+    print(accuracy_score(y_test, y_pred_l1))
+    print(f1_score(y_test, y_pred_l1, average='weighted'))
+    print(confusion_matrix(y_test, y_pred_l1, labels=['PERSON', 'GPE_NORP', 'ORG', 'DATE', 'PERCENT_CARDINAL_MONEY']))
+    '''

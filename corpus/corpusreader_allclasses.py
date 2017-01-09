79d6424d79220ce6bf65727e495c2b8a641d2429
mavicc <mavicc@web.de>
2017-01-09 09:36:11 +0100

    added corpusreader for all classes

--- corpus/corpusreader_allclasses.py
+++ corpus/corpusreader_allclasses.py
@@ -2,7 +2,6 @@
 # Phillip Richter-Pechanski
 # Created on 02.12.2016
 # Example:
-#
 # Build an instance of the Conll_Extraction Reader Class.
 # cr = CorpusReader("/home/tom/Dokumente/UniWiSe2016/ML/ml_ner/wsj/")
 # ne = cr.extract_labeled_named_entities()

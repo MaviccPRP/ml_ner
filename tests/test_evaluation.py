1b98951819df937dfb48949b140818b129b346ca
richter <richter@last.cl.uni-heidelberg.de>
2017-01-13 12:01:17 +0100

    define features in eval

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+sys.stdout.flush()
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 
@@ -19,13 +20,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 
-from sklearn.preprocessing import label_binarize
-from sklearn.metrics import hinge_loss
-from sklearn.metrics import roc_curve, auc
-import matplotlib.pyplot as plt
-from scipy import interp
-from itertools import cycle
-
 from ml_ner.feature_extraction.feature_extractor import FeatureExtractor
 from ml_ner.feature_extraction.arff_scikitdata_creator import ArffAndSciKitDataCreator
 from ml_ner.corpus.corpusreader import CorpusReader
@@ -36,7 +30,7 @@ import numpy as np
 
 extract_features = ['is_np','is_in_wiki','is_title','is_all_caps','is_name','is_com_name','contains_dash','contains_digit','lemma','context','pos']
 
-
+i = 0
 
 for L in range(0, len(extract_features)+1):
     for subset in combinations(extract_features, L):
@@ -46,12 +40,12 @@ for L in range(0, len(extract_features)+1):
 
         # /resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/train/data/english/annotations/nw/
         # Create an instance of the CorpusReader class
-        cr = CorpusReader("/resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/test/data/english/annotations/nw/wsj", "auto")
+        cr = CorpusReader("/resources/corpora/multilingual/ontonotes-5.0-conll-2012/conll-2012/v4/data/train/data/english/annotations/nw/wsj", "auto")
 
         # Extract the NE, its POS tags and phrases
         ne = cr.extract_labeled_named_entities()
 
-        fe = FeatureExtractor(ne, 'train', extract_features, False, True)
+        fe = FeatureExtractor(ne, 'train', subset, False, True)
 
         # Extract features
         samples = fe.extract_all_features()
@@ -73,7 +67,7 @@ for L in range(0, len(extract_features)+1):
         # Extract the NE, its POS tags and phrases
         ne = cr.extract_labeled_named_entities()
 
-        fe = FeatureExtractor(ne, 'train', extract_features,False, True)
+        fe = FeatureExtractor(ne, 'train', subset,False, True)
 
         # Extract features
         samples = fe.extract_all_features()
@@ -86,21 +80,13 @@ for L in range(0, len(extract_features)+1):
 
 
         labels=['PERSON', 'GPE_NORP', 'ORG', 'DATE', 'PERCENT_CARDINAL_MONEY']
-        classifier = OneVsRestClassifier(LinearSVC())
-        y_pred = classifier.fit(X_train, y_train).predict(X_test)
-        print(classification_report(y_test, y_pred, target_names=labels, digits=4))
 
-
-        # Binarize the output
-        y_test = label_binarize(y_test, classes=labels)
-        n_classes = y_test.shape[1]
-
-
-        y_train = label_binarize(y_train, classes=labels)
 
         # Learn to predict each class against the other
-        classifier = OneVsRestClassifier(LinearSVC())
+        classifier = OneVsOneClassifier(LinearSVC())
         y_pred = classifier.fit(X_train, y_train).predict(X_test)
+        print("Evaluation no: ", i)
+        i = i + 1
         print(subset)
         print("Accuracy: ", accuracy_score(y_test, y_pred))
         print("F1-Score: ", f1_score(y_test, y_pred, average='weighted'))
